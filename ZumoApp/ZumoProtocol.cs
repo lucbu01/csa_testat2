c57abeb4e9fdedd803e7543394cd09b473d0031c
Luca Bucher <luca.bucher@stud.hslu.ch>
2026-05-18 19:40:17 +0200

    refactoring

--- ZumoApp/ZumoProtocol.cs
+++ ZumoApp/ZumoProtocol.cs
@@ -1,32 +1,25 @@
-﻿using System.Runtime.CompilerServices;
-using ZumoLib;
+﻿namespace ZumoApp;
 
-namespace ZumoApp {
-    public class ZumoProtocol
+public class ZumoProtocol
+{
+    public static void Write(string content)
     {
-
-        public static void Write(string content)
+        var homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        var targetDir = Path.Combine(homePath, ".zumo");
+        var filePath = Path.Combine(targetDir, "protocol.txt");
+        try
         {
-            string homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            string targetDir = Path.Combine(homePath, ".zumo");
-            string filePath = Path.Combine(targetDir, "protocol.txt");
-            try 
-            {
-                if (!Directory.Exists(targetDir))
-                {
-                    Directory.CreateDirectory(targetDir);
-                }
+            if (!Directory.Exists(targetDir)) Directory.CreateDirectory(targetDir);
 
-                // 4. In die Datei schreiben (hängt Text an, falls Datei schon existiert)
-                string timestamp = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
-                File.WriteAllText(filePath, $"// Lino Meyer, Luca Bucher\n// {timestamp}\n{content}");
+            // 4. In die Datei schreiben (hängt Text an, falls Datei schon existiert)
+            var timestamp = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+            File.WriteAllText(filePath, $"// Lino Meyer, Luca Bucher\n// {timestamp}\n{content}");
 
-                Console.WriteLine($"Erfolgreich gespeichert unter: {filePath}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Fehler beim Schreiben der Datei: {ex.Message}");
-            }
+            Console.WriteLine($"Successfully saved: {filePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Fehler beim Schreiben der Datei: {ex.Message}");
         }
     }
 }
